56f749d61f046223e14fadfa21330a4da8815889
olan.hall@icloud.com <meandme1>
2015-09-07 17:46:28 -0400

    Initial Commit

---
+++ RotationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RotationTest
+//
+//  Created by Olan Hall on 9/7/15.
+//  Copyright (c) 2015 Solaric Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RotationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RotationTest
+//
+//  Created by Olan Hall on 9/7/15.
+//  Copyright (c) 2015 Solaric Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
